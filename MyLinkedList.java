a4fc82cf28477e2eff5994d1192a5bc2aa524b03
Kevin Li <kevinli4321@gmail.com>
2019-03-31 11:26:31 -0400

    Es for MyLinkedList as well

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,7 +1,7 @@
-public class MyLinkedList {
+public class MyLinkedList<E> {
   private int m_size;
-  private Node m_start;
-  private Node m_end;
+  private Node<E> m_start;
+  private Node<E> m_end;
 
   //Start with empty MyLinkedList and work up from here.
   public MyLinkedList() {
@@ -18,7 +18,7 @@ public class MyLinkedList {
 
   //As explained in class, only MyLinkedList has access to all Nodes. Anything that
   //returns Nodes should be private and only be used right in this class.
-  private Node getNthNode(int index) {
+  private Node<E> getNthNode(int index) {
     if (index == 0) //Whole point of LinkedList: easy access to beginning/end
       return m_start;
 
@@ -31,7 +31,7 @@ public class MyLinkedList {
     else if (index == m_size - 2)
       return m_end.prev();
 
-    Node current = m_start; //Start from start and loop through nodes
+    Node<E> current = m_start; //Start from start and loop through nodes
     int i = 0;
     while (i < index) { //Keep calling for the next node until the index before, calls a final next() for desired index
       current = current.next();
@@ -43,12 +43,12 @@ public class MyLinkedList {
 
   public boolean add(E value) {
     if (m_size == 0) { //If it's an empty list I have to put in a start
-      m_start = new Node(value, null, null); //One item, nothing to left or right
+      m_start = new Node<E>(value, null, null); //One item, nothing to left or right
       m_end = m_start; //Originally I made a new Node here. Thanks Ethan for tipping me off on this!
     }
 
     else {
-      m_end.setNext(new Node(value, null, m_end));
+      m_end.setNext(new Node<E>(value, null, m_end));
       m_end = m_end.next(); //Thanks Ethan again for pointing out that I kinda need to set a new end
       m_end.setPrev(m_end.prev());
     }
@@ -83,25 +83,25 @@ public class MyLinkedList {
     return output;
   }
 
-  public Integer get(int index) {
+  public E get(int index) {
     if (index < 0 || index >= m_size)
       throw new IndexOutOfBoundsException("Please specify a valid index.");
 
     return getNthNode(index).getData(); //Get nth node, so that I can run Node.getData() on it
   }
 
-  public Integer set(int index, Integer value) {
+  public E set(int index, E value) {
     if (index < 0 || index >= m_size)
       throw new IndexOutOfBoundsException("Please specify a valid index.");
 
-    Integer temp = getNthNode(index).getData(); //Get nth node, so that I can run Node.getData() on it and return it later
+    E temp = getNthNode(index).getData(); //Get nth node, so that I can run Node.getData() on it and return it later
 
-    getNthNode(index).setData(value); //Gets nth node, so that I can run Node.setData(Integer value) on it
+    getNthNode(index).setData(value); //Gets nth node, so that I can run Node.setData(E value) on it
 
     return temp; //I have to return old value to adhere to List interface
   }
 
-  public boolean contains(Integer value) {
+  public boolean contains(E value) {
     for (int i = 0; i < m_size; ++i) //Loop through linkedlist
       if (get(i).equals(value)) //.equals compares 2 objects
         return true;
@@ -109,7 +109,7 @@ public class MyLinkedList {
     return false;
   }
 
-  public int indexOf(Integer value) { //similar to contains() but returns an index
+  public int indexOf(E value) { //similar to contains() but returns an index
     for (int i = 0; i < m_size; ++i)
       if (get(i).equals(value))
         return i;
@@ -117,17 +117,17 @@ public class MyLinkedList {
     return -1; //No value found in LinkedList
   }
 
-  public void add(int index, Integer value) {
+  public void add(int index, E value) {
     if (index < 0 || index > m_size)
       throw new IndexOutOfBoundsException("Please specify a valid index.");
 
     if (m_size == 0) { //from boolean add()
-      m_start = new Node(value, null, null);
+      m_start = new Node<E>(value, null, null);
       m_end = m_start;
     }
 
     else if (index == 0) { //Adding to before the list's first element
-      m_start.setPrev(new Node(value, m_start, null));
+      m_start.setPrev(new Node<E>(value, m_start, null));
       m_start = m_start.prev();
     }
 
@@ -137,19 +137,19 @@ public class MyLinkedList {
 
     else {
       //Links forwards and backwards, with the new Node in the middle
-      Node temp = getNthNode(index); //suggestion from class to only run this once
-      temp.prev().setNext(new Node(value, temp, temp.prev()));
+      Node<E> temp = getNthNode(index); //suggestion from class to only run this once
+      temp.prev().setNext(new Node<E>(value, temp, temp.prev()));
       temp.setPrev(temp.prev().next());
     }
 
     ++m_size;
   }
 
-  public Integer remove(int index) {
+  public E remove(int index) {
     if (index < 0 || index >= m_size || m_size == 0)
       throw new IndexOutOfBoundsException("Please specify a valid index.");
 
-    Integer temp = get(index);
+    E temp = get(index);
 
     if (index == 0)
       m_start = m_start.next();
@@ -158,7 +158,7 @@ public class MyLinkedList {
       m_end = m_end.prev();
 
     else {
-      Node tempNode = getNthNode(index); //suggestion from class
+      Node<E> tempNode = getNthNode(index); //suggestion from class
       tempNode.prev().setNext(tempNode.next());
       tempNode.next().setPrev(tempNode.prev());
     }
@@ -167,7 +167,7 @@ public class MyLinkedList {
     return temp;
   }
 
-  boolean remove(Integer value) {
+  public boolean remove(E value) {
     int indexValue = indexOf(value); //make this a variable so I don't have to run this again in line 157
     if (indexValue == -1)
       return false;
@@ -176,7 +176,7 @@ public class MyLinkedList {
     return true;
   }
 
-  public void extend(MyLinkedList other) {
+  public void extend(MyLinkedList<E> other) {
     m_size += other.size(); //Since I'm not running add I need to manually increase the size
     m_end.setNext(other.m_start);
     other.m_start.setPrev(m_end);
