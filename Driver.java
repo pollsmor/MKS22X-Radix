7303ebde16b82701c63aea93e10db2d5ecae7b3c
Kevin Li <kevinli4321@gmail.com>
2019-04-02 19:56:23 -0400

    Got the bucket system down

--- Driver.java
+++ Driver.java
@@ -1,6 +1,6 @@
 public class Driver {
   public static void main(String[] args) {
-    int[] test = {1, 3, 90, 9841, 7519, 958};
+    int[] test = {3921, 3, 90, 9841, 7519, 958, 70};
     Radix.radixsort(test);
   }
 }
--- Radix.java
+++ Radix.java
@@ -6,40 +6,57 @@ public class Radix {
     boolean digitsLeft = true;
     int digitsFromRight = 1;
     MyLinkedList<Integer> numbers = new MyLinkedList<Integer>();
+    for (int i = 0; i < data.length; ++i)
+      numbers.add(data[i]);
+
     while (digitsLeft) {
       digitsLeft = false;
-      for (int i = 0; i < data.length; ++i) {
-        numbers.add(data[i]);
-      }
 
       @SuppressWarnings("unchecked")
       MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
+      for (int i = 0; i < 20; ++i)
+        buckets[i] = new MyLinkedList<Integer>();
+
       while (numbers.size() > 0) {
         int num = numbers.removeFront();
-        System.out.println(getNthDigit(num, digitsFromRight));
         if (Math.abs(num) == num) { //positive #
           buckets[getNthDigit(num, digitsFromRight) + 10].add(num); //thx Jerry for explanation, would've done 10 conditionals otherwise
           digitsLeft = true;
         }
 
-        else {//negative #
+        else { //negative #
           buckets[9 - getNthDigit(num, digitsFromRight)].add(num);
           digitsLeft = true;
         }
       }
 
       ++digitsFromRight;
+      for (int i = 0; i < buckets.length; ++i)
+        System.out.println(buckets[i]);
+
+      for (int i = 0; i < buckets.length; ++i) {
+        while (buckets[i].size() > 0) {
+          //System.out.println("lol");
+          int num = buckets[i].removeFront();
+          //System.out.println(num);
+          numbers.add(num);
+        }
+      }
     }
 
-    System.out.println(numbers);
+    for (int i = 0; i < data.length; ++i)
+      data[i] = numbers.removeFront();
+
+    System.out.println(Arrays.toString(data));
   }
 
-  private static int getNthDigit(int input, int digitsFromRight) {
+  public static int getNthDigit(int input, int digitsFromRight) {
     String num = String.valueOf(input);
     char[] digits = num.toCharArray();
 
-    if (digits.length - digitsFromRight >= 0)
-      return digits[digits.length - digitsFromRight];
+    if (digits.length - digitsFromRight >= 0) {
+      return Character.getNumericValue(digits[digits.length - digitsFromRight]);
+    }
 
     return 0;
   }
